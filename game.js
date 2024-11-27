5e21f1993cfc1883d600f4b6ebd32d1cb37ee162
digeesh038 <digeesh038@gmail.com>
2024-11-28 00:27:00 +0530

    d

--- game.js
+++ game.js
@@ -14,8 +14,8 @@ import CollisionBranch from "./assets/CollisionBranch.js";
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
-        this.timer = 61;
-        this.maxTime = 61;
+        this.timer = 1;
+        this.maxTime = 1;
         this.elapsedTime = 0;
         this.score = 0;
     }
